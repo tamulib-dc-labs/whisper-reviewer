24020d67926b68e29879a605c01fa21f0c7b6d89
Mark Baggett <markpbaggett@gmail.com>
2025-03-24 14:42:49 -0500

    Build at subdomain.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/whisper-reviewer/',
+  build: {
+    rollupOptions: {
+      input: './index.html', // Ensure this points to your actual index.html
+    }
+  }
 });
