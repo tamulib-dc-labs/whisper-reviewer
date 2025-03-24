07f4499772c36a6d2cda4e5683db3814fc34ad37
Mark Baggett <markpbaggett@gmail.com>
2025-03-24 14:57:02 -0500

    One more try.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Fetch the media JSON file
-  fetch("./test_for_whisper_transcript_online.json")
+  fetch("/whisper-reviewer//test_for_whisper_transcript_online.json")
     .then((response) => response.json())
     .then((mediaFiles: MediaFile[]) => {
       // Create the dropdown
